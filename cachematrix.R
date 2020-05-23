ad968ae005d269f15c20c7eb214d7c6edb97514f
Eduard Korchmar <korchmar.eduard@gmail.com>
2020-05-24 01:01:10 +0300

    Written makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create tool to cache of solution of a matrix
+## Usage: cacheSolve(makeCacheMatrix(m1))
+## Where m1 is source matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix() stores matrix in it's environment and provides ways to cache solution of that matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  ##x is initialized in function call, initialize m (future cache)
+  m <- NULL
+  ##set: initialize y and reassign vallue of x in parent environment; clear cache
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ##get: return x
+  get <- function() x
+  ##setsolve: initialize solution of the matrix to keep it cached in m
+  setsolve <- function(solve) m <<- solve
+  ##getsolve: return cached m
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'. Uses cache of makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Initiate m to be checked from cache
+  m <- x$getsolve()
+  if(!is.null(m)){
+    message("Solved matrix found in cache")
+    return(m)
+  }
+  ## If cache is empty, solve matrix and store it in cache
+  data <- x$get()
+  m <- solve(data,...)
+  x$setsolve(m)
+  m
 }
